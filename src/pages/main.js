826231397d715179774c596b56c7d8ba086200b2
parkdaxun <parkde0207@gmail.com>
2022-08-03 16:50:20 +0900

    [feat] + buttons

--- src/pages/main.js
+++ src/pages/main.js
@@ -12,10 +12,16 @@ function main() {
                 <a href="https://play.google.com/store/apps/details?id=com.croquis.zigzag&referrer=af_tranid%3Dx8qDlPq9-TEjlPdkZop1QQ%26shortlink%3D189189a0%26pid%3Dzigzag_website_aos%26af_click_lookback%3D1d%26af_web_id%3D6332b30f-22ab-4221-8322-80e06e8c4f97-p%26utm_source%3Dzigzag_website_aos">
                     <img className="google" src={require("../image/google_play.png")} />
                 </a>
-                <img className="picture" src={require("../image/picture2.png")} />
-                <img className="picture" src={require("../image/picture3.png")} />
-                <img className="picture" src={require("../image/picture4.png")} />
-                <img className="picture" src={require("../image/picture5.png")} />
+                <img className="picture2" src={require("../image/picture2.png")} />
+                <a href="https://zigzag.kr/partners/">
+                    <img className="new" src={require("../image/new.png")} />
+                </a>
+                <a href="https://shop.zigzag.kr/#!/login">
+                    <img className="login" src={require("../image/login.png")} />
+                </a>
+                {/*<img className="picture3" src={require("../image/picture3.png")} />*/}
+                {/*<img className="picture4" src={require("../image/picture4.png")} />*/}
+                {/*<img className="picture5" src={require("../image/picture5.png")} />*/}
             </div>
 
         </div>
